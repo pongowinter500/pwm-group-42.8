a9ed3ff5d05eacf05d6a866838763f4880abb488
Pedro-Canarias <three.guys.in.galway@gmail.com>
2026-04-28 19:36:20 +0100

    firebase installed

--- app_angular/src/app/app.config.ts
+++ app_angular/src/app/app.config.ts
@@ -1,12 +1,16 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
-
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
 import { routes } from './app.routes';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth())
   ]
 };
